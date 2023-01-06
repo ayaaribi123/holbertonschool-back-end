0de36b826eb90d5a654af1ba7f68d61ad3a8002f
ayaaribi123 <ayaaribi11@gmail.com>
2023-01-06 13:21:56 +0100

    test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,19 +2,17 @@
 """return todo list progress"""
 import requests
 import sys
-if __name__ == '__main__':
-    user = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])).json()
-    todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos",
-        params={
-            "userId": sys.argv[1]}).json()
 
-completed = []
-for task in todos:
-    if task.get("completed") is True:
-        completed.append(task.get("title"))
-print("Employee {} is done with tasks({}/{}):".format(
-    user.get("name"), len(completed), len(todos)))
-for complete in completed:
-    print("\t {}".format(complete))
+if __name__ == '__main__':
+    api_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(api_url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(
+        api_url + "todos", params={"userId": sys.argv[1]}).json()
+    completed = []
+    for task in todos:
+        if task.get("completed") is True:
+            completed.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    for complete in completed:
+        print("\t {}".format(complete))
